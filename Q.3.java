5af6fccccfb35b7934094e35631780336b53c3ab
user-default <71413474+user-default@users.noreply.github.com>
2020-09-25 14:50:16 -0300

    Q.3.java

---
+++ Q.3.java
@@ -0,0 +1,22 @@
+class Q.3 
+{
+  public static void main(String[] args) 
+  {
+    //Esse software está muito pesado, é necessário fazer algumas alterações vizando a otimização.
+    String str1 = "Bosco";
+    String str2 = "Aurelia";
+    String str3 = "Derllane";
+    String str4 = "Rogerio";
+    String str5 = "Damião";
+    
+    if (str1.compareTo(str2) < 0 && str1.compareTo(str3) < 0 && str1.compareTo(str4) < 0 && str1.compareTo(str5) < 0) System.out.println("str1 é a menor de todas.\n");
+
+    else if (str2.compareTo(str1) < 0 && str2.compareTo(str3) < 0 && str2.compareTo(str4) < 0 && str2.compareTo(str5) < 0) System.out.println("str2 é a menor de todas.\n");
+
+    else if (str3.compareTo(str2) < 0 && str3.compareTo(str4) < 0 && str3.compareTo(str5) < 0) System.out.println("str3 é a menor de todas.\n");
+
+    else if (str4.compareTo(str2) < 0 && str4.compareTo(str3) < 0 && str4.compareTo(str5) < 0) System.out.println("str4 é a menor de todas.\n");
+
+   else System.out.println("str5 é a menor de todas.\n");
+  }
+}
