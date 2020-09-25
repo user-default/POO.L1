da2a4f941c464073659716f4018134a5f875131f
user-default <71413474+user-default@users.noreply.github.com>
2020-09-25 15:01:33 -0300

    Q9.java

---
+++ Q9.java
@@ -0,0 +1,10 @@
+class Q9 // Imprime números par entre 1 e 20. 
+{
+  public static void main(String[] args) 
+  {
+    for (int i = 1; i <= 20; i++)
+    {
+      if(i % 2 == 0) System.out.printf("%d\n", i);
+    }
+  }
+}
